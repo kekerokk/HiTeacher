73edb08788da9310075fe2f8db662ea0518978e8
Иванчок <@DESKTOP-L0B0H3T>
2022-03-08 20:17:23 +0300

    Добавьте файлы проекта.

---
+++ HiTeacher/Program.cs
@@ -0,0 +1,5 @@
+﻿using GeometrusAkulovIvan;
+
+CubeMath cube1 = new CubeMath();
+Console.WriteLine($"Площадь куба: {cube1.S(3)}\nОбъем куба: {cube1.V(3)}\nЧисло в квадрате деленное на 2: {cube1.math(5)}");
+
---
+++ MyLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace GeometrusAkulovIvan
+{
+    public class CubeMath
+    {
+        public double S(float a)
+        {
+            double S = Math.Pow(a,2);
+            return S;
+        }
+        public double V(float a)
+        {
+            double V = S(a) * a;
+            return V;
+        }
+        
+        public double math(float number)
+        {
+            return Math.Pow(number, 3) / 2;
+        }
+    }
+}
